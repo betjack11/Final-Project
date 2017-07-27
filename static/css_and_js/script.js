651db3d7ac9a70e34f1aa44ec7d983a7bf4bb8b9
KatMasters <demouser@MacBook-Pro.local>
2017-07-27 11:31:14 -0400

    Third Commit

--- static/css_and_js/script.js
+++ static/css_and_js/script.js
@@ -1,8 +1,27 @@
-function isCorrect(){
-  if($(".correct").attr("checked") == "checked"){
-    var para = document.createElement("P");
-    var answer = document.createTextNode("That was the correct choice");
-    para.appendChild(answer);
-    document.body.appendChild(para);
+var score = 0;
+
+function getScore(){
+  if($(".question1.correct").attr("checked") == "checked"){
+    score++;
   }
+  if($(".question2.correct").attr("checked") == "checked"){
+    score++;
+  }
+  if($(".question3.correct").attr("checked") == "checked"){
+    score++;
+  }
+  if($(".question4.correct").attr("checked") == "checked"){
+    score++;
+  }
+  if($(".question5.correct").attr("checked") == "checked"){
+    score++;
+  }
+  printScore();
+}
+
+function printScore(){
+  var para = document.createElement("P");
+  var current_score = document.createTextNode("Score : " + score);
+  para.appendChild(current_score);
+  document.getElementById("questions").appendChild(para);
 }
